d3a2cc818ebe58901885562bcb74b7c5b89138ba
cui0523 <144660932+cui0523@users.noreply.github.com>
2026-03-17 09:47:51 +0800

    Update text2text_generation_demo.py

--- test-ci/text2text_generation_demo.py
+++ test-ci/text2text_generation_demo.py
@@ -1,6 +1,7 @@
 from mindnlp.transformers import pipeline
 
 generator = pipeline("text2text-generation", model="t5-base")
+import mindspore
 outputs = generator("My name is Wolfgang and I live in Berlin")
 print(outputs)
 # [{'generated_text': 'Wolfgang and I live in Berlin. My name is Wolfgang and I am from Berlin.'}]
